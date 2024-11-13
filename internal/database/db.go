f7a200df64546f0587765e4880a101d679521f62
Akshat Gupta <akshat.g1707@gmail.com>
2024-11-14 00:20:56 +0530

    fix: db

--- internal/database/db.go
+++ internal/database/db.go
@@ -5,7 +5,8 @@ import (
 	"log"
 	"os"
 	"strconv"
-    "github.com/Oik17/file-sharing-system/internal/utils"
+
+	"github.com/Oik17/file-sharing-system/internal/utils"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq" // Import the PostgreSQL driver
 )
@@ -50,6 +51,22 @@ func Connect() {
 func runMigrations(db *sqlx.DB) {
 	_, err := db.Exec(`
 		CREATE TABLE IF NOT EXISTS test (test VARCHAR(255));
+		
+		CREATE TABLE IF NOT EXISTS users (
+			id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+			email VARCHAR(255) UNIQUE NOT NULL,
+			username VARCHAR(255) UNIQUE NOT NULL,
+			profile_picture TEXT,
+			verified_email BOOLEAN DEFAULT FALSE,
+			files TEXT[],
+			storage_quota BIGINT DEFAULT 0,
+			storage_used BIGINT DEFAULT 0,
+			created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+			updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+			access_token TEXT,
+			refresh_token TEXT
+);
+
 	`)
 
 	if err != nil {
--- internal/models/userModel.go
+++ internal/models/userModel.go
@@ -1,22 +1,22 @@
-package models
+	package models
 
-import (
-	"time"
+	import (
+		"time"
 
-	"github.com/google/uuid"
-)
+		"github.com/google/uuid"
+	)
 
-type User struct {
-	ID             uuid.UUID `json:"id" db:"id"`
-	Email          string    `json:"email" db:"email"`
-	Username       string    `json:"username" db:"username"`
-	ProfilePicture string    `json:"profile_picture" db:"profile_picture"`
-	VerifiedEmail  bool      `json:"verified_email" db:"verified_email"`
-	Files          []string  `json:"files" db:"files"`
-	StorageQuota   int64     `json:"storage_quota" db:"storage_quota"` // in bytes
-	StorageUsed    int64     `json:"storage_used" db:"storage_used"`   // in bytes
-	CreatedAt      time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt      time.Time `json:"updated_at" db:"updated_at"`
-	AccessToken    string    `json:"access_token" db:"access_token"`
-	RefreshToken   string    `json:"refresh_token" db:"refresh_token"`
-}
+	type User struct {
+		ID             uuid.UUID `json:"id" db:"id"`
+		Email          string    `json:"email" db:"email"`
+		Username       string    `json:"username" db:"username"`
+		ProfilePicture string    `json:"profile_picture" db:"profile_picture"`
+		VerifiedEmail  bool      `json:"verified_email" db:"verified_email"`
+		Files          []string  `json:"files" db:"files"`
+		StorageQuota   int64     `json:"storage_quota" db:"storage_quota"` // in bytes
+		StorageUsed    int64     `json:"storage_used" db:"storage_used"`   // in bytes
+		CreatedAt      time.Time `json:"created_at" db:"created_at"`
+		UpdatedAt      time.Time `json:"updated_at" db:"updated_at"`
+		AccessToken    string    `json:"access_token" db:"access_token"`
+		RefreshToken   string    `json:"refresh_token" db:"refresh_token"`
+	}

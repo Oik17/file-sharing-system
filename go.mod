ffa3cd11490aa7c8da73ba3917a3a10bf0bb27e2
Akshat Gupta <akshat.g1707@gmail.com>
2025-03-25 12:59:18 +0530

    feat: redis

--- go.mod
+++ go.mod
@@ -11,12 +11,15 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/lib/pq v1.10.9
+	github.com/redis/go-redis/v9 v9.7.3
 	golang.org/x/oauth2 v0.24.0
 )
 
 require (
 	cloud.google.com/go/compute/metadata v0.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/creasty/defaults v1.7.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/gorilla/schema v1.4.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ internal/database/redisConfig.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"context"
+	"log"
+
+	"github.com/Oik17/file-sharing-system/internal/utils"
+	"github.com/redis/go-redis/v9"
+)
+
+var RedisClient *redis.Client
+
+func InitRedis() {
+	// redisDB, err := strconv.Atoi(utils.Config("REDIS_DB"))
+	// if err != nil {
+	// 	log.Fatalf("Invalid REDIS_DB value: %v", err)
+	// }
+
+	// RedisClient = redis.NewClient(&redis.Options{
+	// 	Addr:     fmt.Sprintf("%s:%s", utils.Config("REDIS_HOST"), utils.Config("REDIS_PORT")),
+	// 	Password: utils.Config("REDIS_PASSWORD"),
+	// 	DB:       redisDB,
+	// })
+
+	opt, _ := redis.ParseURL(utils.Config("REDIS_URI"))
+	RedisClient = redis.NewClient(opt)
+
+	ctx := context.Background()
+
+	err := RedisClient.Set(ctx, "foo", "bar", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := RedisClient.Get(ctx, "foo").Result()
+	if err != nil {
+		panic(err)
+	}
+	log.Println("foo", val)
+	log.Println("Redis Connected")
+}

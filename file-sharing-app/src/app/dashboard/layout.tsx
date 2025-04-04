19c0d3b56cc8048843d5164fc6b20b9e9e406b56
Akshat Gupta <akshat.g1707@gmail.com>
2025-04-05 01:24:44 +0530

    fiX: css

---
+++ file-sharing-app/src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import { Toaster } from 'react-hot-toast';
+
+import { ReactNode } from 'react';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Toaster position="top-right" reverseOrder={false} />
+      </body>
+    </html>
+  );
+}

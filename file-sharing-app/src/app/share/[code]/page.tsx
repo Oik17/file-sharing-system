a96d447b9d7de1462be759f399ba6cfc76d496b8
Akshat Gupta <akshat.g1707@gmail.com>
2025-04-06 22:21:28 +0530

    es lint

--- file-sharing-app/src/app/share/[code]/page.tsx
+++ file-sharing-app/src/app/share/[code]/page.tsx
@@ -1,6 +1,4 @@
 import SharedFile from '@/components/SharedFile';
-// import { type Metadata } from 'next';
-
 
 export default function SharePage({ params }: { params: { code: string } }) {
   return <SharedFile code={params.code} />;

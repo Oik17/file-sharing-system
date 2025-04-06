4ab80a2bb8341c052e8d67c7770d6b5e5b0a8e2e
Akshat Gupta <akshat.g1707@gmail.com>
2025-04-06 22:29:50 +0530

    fix

--- file-sharing-app/src/app/share/[code]/page.tsx
+++ file-sharing-app/src/app/share/[code]/page.tsx
@@ -1,5 +1,8 @@
 import SharedFile from '@/components/SharedFile';
 
-export default function SharePage({ params }: { params: { code: string } }) {
-  return <SharedFile code={params.code} />;
+type Params = Promise<{ code: string }>;
+
+export default async function SharePage({ params }: { params: Params }) {
+  const { code } = await params;
+  return <SharedFile code={code} />;
 }

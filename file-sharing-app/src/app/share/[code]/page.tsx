71d50383c430c75f81768312e615d3d1850e934a
Akshat Gupta <akshat.g1707@gmail.com>
2025-04-06 22:15:48 +0530

    fix: more eslint issues...................

--- file-sharing-app/src/app/share/[code]/page.tsx
+++ file-sharing-app/src/app/share/[code]/page.tsx
@@ -1,12 +1,9 @@
 import SharedFile from '@/components/SharedFile';
 
-interface SharePageProps {
-  params: {
-    code: string;
-  };
-}
+type SharePageProps = {
+  params: { code: string };
+};
 
 export default function SharePage({ params }: SharePageProps) {
   return <SharedFile code={params.code} />;
 }
-
